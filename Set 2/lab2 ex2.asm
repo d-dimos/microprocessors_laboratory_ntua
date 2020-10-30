c58a48952732de0eee170f3fd5590c45680a790a
Nafsika Abatzi <61847066+nafsika24@users.noreply.github.com>
2020-10-31 00:38:06 +0200

    Update lab2 ex2.asm

--- Set 2/lab2 ex2.asm
+++ Set 2/lab2 ex2.asm
@@ -5,32 +5,32 @@
 ; Lab Exercise Set 2 - Exercise 2
 
 rjmp reset
-.org 0x4	;addr of INT1 is at 0x4
+.org 0x4     ;addr of INT1 is at 0x4
 rjmp ISR1
 
-reset: ldi r24 ,( 1 << ISC11) | ( 1 << ISC10)
+reset: 		ldi r24 ,( 1 << ISC11) | ( 1 << ISC10)
 		out MCUCR , r24 
 		ldi r24 ,( 1 << INT1) ; INT1 is allowed
 		out GICR , r24
 		sei
 
-IO_set: ser r24 ; initialize PORTC
+IO_set:		ser r24 ; initialize PORTC
 		out DDRC, r24 ; for output
 		clr r24 ; initialize PORTA
 		out DDRC, r24	;for input when INT1 occures
 
 
-main_program: out PORTC , r26 ; show counter at PORTC
-			  inc r26 ; increase counter
-	  		  rjmp main_program ; loop
+main_program: 	out PORTC , r26 ; show counter at PORTC
+		  inc r26 ; increase counter
+  		  rjmp main_program ; loop
 
 
 ISR1:	in r21,PINA	;load PINA
-		andi r21,0xc0	;check bits 6 and 7
-		cpi r21,0xc0
-		breq cont	;if bits 6 and 7 ->'1' continue
-		rjmp ISR1	;else wait
+	andi r21,0xc0	;check bits 6 and 7
+	cpi r21,0xc0	
+	breq cont	;if bits 6 and 7 ->'1' continue
+	rjmp ISR1	;else wait
 
 cont:	inc r23	;increase the counter for the interupts INT1 till now
-		out PORTB,r23	;show the counter at PORTB
-		reti
+	out PORTB,r23	;show the counter at PORTB
+	reti
